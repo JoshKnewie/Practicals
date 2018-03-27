3a10106c6c9fdb456e3f7a2b591b49dacb75761d
Josh Knewstub <joshua.knewstub@my.jcu.edu.au>
2018-03-27 12:33:33 +1000

    Change variable names in colour code

--- prac_05/colour_codes.py
+++ prac_05/colour_codes.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical
 State colours in a dictionary
 """
 
-COLOUR_NAME = {"BLUE": "#0000ff", "ORANGE": "#ffa500", "BLACK": "#000000", "BROWN": "#a52a2a",
+COLOUR_NAMES = {"BLUE": "#0000ff", "ORANGE": "#ffa500", "BLACK": "#000000", "BROWN": "#a52a2a",
                "GRAY": "#bebebe", "PINK": "#ffc0cb", "PURPLE": "#a020f0", "RED": "#ff0000", "YELLOW": "#ffff00",
                "WHITE": "#ffffff", "GREEN": "#00ff00"}
 
@@ -11,18 +11,18 @@ COLOUR_NAME = {"BLUE": "#0000ff", "ORANGE": "#ffa500", "BLACK": "#000000", "BROW
 def main():
     colour = input("Enter colour name: ").upper()
     while colour != "":
-        if colour in COLOUR_NAME:
-            print(colour, "code is", COLOUR_NAME[colour])
+        if colour in COLOUR_NAMES:
+            print(colour.capitalize(), "code is", COLOUR_NAMES[colour])
         elif colour == "SHOW ALL":
-            print_states()
+            print_colour()
         else:
             print("Invalid colour name")
         colour = input("Enter colour name: ").upper()
 
 
-def print_states():
-    for colours in COLOUR_NAME:
-        print("{:3} is {}".format(colours, COLOUR_NAME[colours]))
+def print_colour():
+    for colours in COLOUR_NAMES:
+        print("{:3} is {}".format(colours, COLOUR_NAMES[colours]))
 
 
 main()
